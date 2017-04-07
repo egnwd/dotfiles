8a0daca67c39f78efa81bf8af04e9548c8b89ada
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2017-04-07 19:39:04 +0100

    Update git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,10 +14,11 @@ alias gp='git push origin HEAD'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
+alias ga='git add'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias gcb='git checkout -b'
+alias gcopy='git copy-branch-name'
+alias gbr='git branch'
+alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
